fa25904e0944adb9df57500771791a44c2140d5a
Joe Stwart Valencia <fabio.ramirez1996@hotmail.com>
2024-12-21 09:29:52 -0500

    redirect home to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+"use client";
+import { useAuth } from "@/hooks/useAuth";
 import Image from "next/image";
 
 export default function Home() {
+	const { isFetchingUser, isMounted } = useAuth({
+		middleware: "guest",
+		redirectIfAuthenticated: "/dashboard",
+	});
 	return <h1>Hello word!</h1>;
 }
